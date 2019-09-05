b02ae0b76e2a3770eb9298632031416114838535
rtestard <rtestard@opensense.fr>
2019-09-05 09:20:21 +0200

    I have added links to check if relative path finding

--- src/components/Bar.js
+++ src/components/Bar.js
@@ -1,7 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Bar = () => {
-  return <div>Bar Component</div>;
+  return (
+    <div>
+      Bar Component <Link to="/foo">Foo</Link>
+    </div>
+  );
 };
 
 export default Bar;
--- src/components/Foo.js
+++ src/components/Foo.js
@@ -1,7 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Foo = () => {
-  return <div>Foo Component</div>;
+  return (
+    <div>
+      Foo Component <Link to="/bar">Bar</Link>
+    </div>
+  );
 };
 
 export default Foo;
